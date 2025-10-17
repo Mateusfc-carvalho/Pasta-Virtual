964e4cb23d343e7b05ba442b0b4b24226bf98657
Mateus Carvalho <Mateusfc2004@gmail.com>
2025-10-17 17:24:35 -0300

    resolvendo alguns bugs, e inicializando area de adicionar Músicas

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 // vite.config.js
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression'; // Importa o plugin
 
 export default defineConfig(({ mode }) => ({
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 10240,
+      deleteOriginalAssets: false,
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 10240,
+      deleteOriginalAssets: false,
+    }),
+  ],
   base: mode === 'production' ? '/pasta-virtual/' : '/',
   build: {
     outDir: 'dist',
@@ -14,6 +29,6 @@ export default defineConfig(({ mode }) => ({
     port: 5173,
     open: true,
   },
-}))
+}));
 
 
